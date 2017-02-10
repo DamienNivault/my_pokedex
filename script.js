3fbdf9f44a18613b1096389f6fdc9a6d99697bda
Damien Nivault <damien.nivault@supinternet.fr>
2017-02-10 13:48:19 +0100

    first commit Pokedex

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
 window.onload = function () {
     var form = document.forms['form'];
     var name = document.querySelector('#pokemonName');
-    var type = document.querySelector('#type');
-    var errorBlock = document.querySelector('#blockError');
-    var contentImg = document.querySelector('#content_img');
+    var type = document.querySelector('#pokemonType');
+    var circleValid = document.querySelector('#circleContainerImg');
+    var errorBlock = document.querySelector('#container_pokemon_name');
+    var contentImg = document.querySelector('#containerBlack');
+   // var buttonRight =  document.querySelector('#buttonRight');
     var pokemonReq = new XMLHttpRequest();
     pokemonReq.onload = reqListener();
     pokemonReq.open("get", "pokemons.json", true);
     pokemonReq.send();
-
-
     function reqListener() {
         form.onsubmit = function () {
-            var pokemon = document.querySelector('#search').value;
+            var pokemon = this.elements['search'].value;
+            var maj = pokemon.charAt(0).toUpperCase();
+            var pokeName = maj + pokemon.substring(1);
             var obj = JSON.parse(pokemonReq.responseText);
             var pokeValid= false;
             for (var i in obj) {
-                if (obj[i].name == pokemon || i == pokemon) {
+                if (obj[i].name == pokeName || i == pokeName) {
                     pokeValid = true;
                     errorBlock.innerHTML='';
                     name.innerHTML = (obj[i].name);
                     type.innerHTML = (obj[i].type);
-                    contentImg.innerHTML = '<img class="img" src="http://img.pokemondb.net/artwork/' + obj[i].name.toLowerCase().replace(". ", "-") + '.jpg">'; // replace for Mr Mime
+                    contentImg.innerHTML = '<img class="img" src="http://img.pokemondb.net/artwork/' + obj[i].name.toLowerCase().replace(". ","-").replace("\'","") + '.jpg">';
+                    circleValid.style.background="green";
+                   /* buttonRight.onclick= function (){
+                        contentImg.innerHTML = '<img class="img" src="http://img.pokemondb.net/artwork/' + obj[i+1].name.toLowerCase().replace(". ","-").replace("\'","") + '.jpg">';
+                        console.log(obj[i]);
+                    }*/
                 }
             }
             if(pokeValid === false) {
                 if(isNaN(pokemon))
                 {
-                    errorBlock.innerHTML = pokemon + 'not found';
+                    errorBlock.classList.add('errorBlock');
+                    errorBlock.innerHTML = pokeName + ' not found';
                     name.innerHTML = '';
                     type.innerHTML = '';
                     contentImg.innerHTML = '';
+                    circleValid.style.background="red";
+                    contentImg.innerHTML = '<img class="imgError" src="http://image.noelshack.com/fichiers/2014/28/1405038568-063.png">';
                 }else{
-                    errorBlock.innerHTML = 'Pokémon number' +pokemon+' not found' ;
+
+                    errorBlock.classList.add('errorBlock');
+                    errorBlock.innerHTML = 'Pokémon number ' +pokeName+ ' not found' ;
                     name.innerHTML = '';
                     type.innerHTML = '';
                     contentImg.innerHTML = '';
+                    circleValid.style.background="red";
+                    contentImg.innerHTML = '<img class="imgError" src="http://image.noelshack.com/fichiers/2014/28/1405038568-063.png">';
                 }
             }
             return false;
